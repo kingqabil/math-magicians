616bb3810c8cf24d5ef7bb2627368f82f8fe36fc
kingqabil <kingqabil@live.com>
2022-01-19 20:04:30 +0300

    Added  test and styling

---
+++ src/components/Quote.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Nav from './Nav';
+
+const Quote = () => (
+  <>
+    <Nav />
+    <div className="home-wrapper">
+      <p className="quote">
+        Go down deep enough into anything and you will find mathematics. --Dean Schlicter
+      </p>
+    </div>
+  </>
+);
+
+export default Quote;
